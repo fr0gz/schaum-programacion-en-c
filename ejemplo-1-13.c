9be9d4ba4017687bac24d14efdc7bfbb6cd338f7
fr0gz <cguevarak@pm.me>
2020-08-28 14:37:13 -0400

    Resolvi problema en ejemplo 1 13, notar que el warning tenia que ver con %s

--- ejemplo-1-13.c
+++ ejemplo-1-13.c
@@ -16,4 +16,36 @@ int main()
 		char texto[20];
 		float radio;
 		float area;
-		
+	} circulo[10];
+	
+	float procesar(float radio);
+
+	printf("Para PARAR, introducir FIN en el identificador\n");
+	printf("\nIdentificador: ");
+	scanf("%s", circulo[i].texto);
+	while (circulo[i].texto[0] != 'F' || circulo[i].texto[1] != 'I' 
+					|| circulo[i].texto[2] != 'N') {
+
+		printf("Radio: ");
+		scanf("%f", &circulo[i].radio);
+
+		if (circulo[i].radio < 0)
+			circulo[i].area = 0;
+		else
+			circulo[i].area = procesar(circulo[i].radio);
+
+		++i;
+		printf("\nIdentificador: ");
+		scanf("%s", circulo[i].texto);
+	}
+
+	n = --i; /* el valor mayor de i */
+
+	printf("\n\nRelacion de resultados\n\n");
+	for(i = 0; i <= n; ++i){
+		printf("%s Radio = %f Area = %f\n", circulo[i].texto,
+						    circulo[i].radio,
+						    circulo[i].area);
+		}
+} 
+							
