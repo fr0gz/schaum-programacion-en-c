13eb7e2a256c065de0b56a56880028528e50a648
fr0gz <cguevarak@pm.me>
2020-08-29 19:30:29 -0400

    Falta integrar la parte de más abajo

---
+++ ejemplo-4-32.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	char nombre[20];
+	float puntos1, puntos2, puntos3, media;
+
+	printf("Por favor, introduce tu nombre: ");
+	scanf("%[^n]", nombre);
+
+	
+
+
+
+
+
+
+	
+
